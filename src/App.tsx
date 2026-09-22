2cf35c1c78a764b1168a5f497c188a744ce58d74
H0ng <ckdals7506@naver.com>
2026-09-22 17:22:36 +0900

    [Fix] 사용자 공개 라우트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { RequireAdmin } from './admin/routes/RequireAdmin'
 import { AuthProvider } from './auth/context/AuthProvider'
 import { LoginPage } from './auth/pages/LoginPage'
 import { UserSignupPage } from './user/auth/pages/UserSignupPage'
-import { UserHomePage } from './user/pages/UserHomePage'
+import UserApp from './user/UserApp'
 import './App.css'
 
 function App() {
@@ -18,20 +18,33 @@ function App() {
     <BrowserRouter>
       <AuthProvider>
         <Routes>
+          {/* 인증 */}
           <Route path="/auth/login" element={<LoginPage />} />
           <Route path="/auth/signup" element={<UserSignupPage />} />
-          <Route path="/user" element={<UserHomePage />} />
+
+          {/* 관리자 - 인증 필요 */}
           <Route element={<RequireAdmin />}>
             <Route path="/admin" element={<AdminLayout />}>
               <Route index element={<Navigate replace to="dashboard" />} />
               <Route path="dashboard" element={<DashboardPage />} />
-              <Route path="faqs" element={<FaqLayout />}><Route index element={<FaqListPage />} /><Route path="deleted" element={<DeletedFaqListPage />} /><Route path="categories" element={<FaqCategoryListPage />} /><Route path=":faqId" element={<FaqDetailPage />} /></Route>
+
+              <Route path="faqs" element={<FaqLayout />}>
+                <Route index element={<FaqListPage />} />
+                <Route path="deleted" element={<DeletedFaqListPage />} />
+                <Route path="categories" element={<FaqCategoryListPage />} />
+                <Route path=":faqId" element={<FaqDetailPage />} />
+              </Route>
+
+              <Route path="*" element={<Navigate replace to="dashboard" />} />
             </Route>
           </Route>
-          <Route path="*" element={<Navigate replace to="/admin" />} />
+
+          {/* 일반 사용자 - 공개 */}
+          <Route path="/*" element={<UserApp />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
   )
 }
+
 export default App
